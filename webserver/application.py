5a9c72a46bd6798101e33729d21360fd2a7ed437
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-11-26 00:08:13 +0530

    Updated admin-page

--- webserver/application.py
+++ webserver/application.py
@@ -2,14 +2,13 @@ import os
 import requests
 from flask import Flask, render_template, request
 
+from util.apiUtil import APIRequest
+
 application = app = Flask(__name__)
 
 # Admin Page
 @app.route("/",methods = ["GET", "POST"])
 def index():
-
-    # Set Error to None
-    error = None
     
     # Initialize empty file list
     file_list = []
@@ -25,14 +24,22 @@ def index():
             
             # POST request to API
             # Check if success
+            obj = APIRequest()
+            response = obj.post("/cache", {
+                                    "filename" : filename,
+                                    "path" : filepath
+                                    })
+            post_success = response["success"]
             
             # GET request to API
+            response = obj.get("/dashboard", {})           
+
             # list of files
-            file_list = ["abcd.pdf", "efgh.png", "ijkl.jpeg", "mnop.md"]
+            file_list = response[0]["message"]["key_list"]
             
             # Response Variable
             res = {
-                'post_success' : True,
+                'post_success' : post_success,
                 'file_list' : file_list,
                 'number_of_files' : len(file_list)
             }
@@ -51,8 +58,11 @@ def index():
         try:
             
             # GET request to API
+            obj = APIRequest()
+            response = obj.get("/dashboard", {}) 
+
             # list of files
-            file_list = ["abcd.pdf", "efgh.png", "ijkl.jpeg", "mnop.md"]
+            file_list = response[0]["message"]["key_list"]
             
             # Response Variable
             res = {
