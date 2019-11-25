8b8e35768dffc78d32aca5f1340093c9fb4874f2
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-11-25 15:37:43 +0530

    Fix: bind-mount

---
+++ app/set_compose_env.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Configure the container for bind-mount
+# Sets variables in .env
+WIN_PATH="/c/Users"
+UBU_PATH="/usr"
+MAC_PATH=""
+
+# Update SOURCE path in .env for docker-compose.yml
+case "$OSTYPE" in
+  darwin*)  echo "SOURCE=$MAC_PATH" > ../.env ;; 
+  linux*)   echo "SOURCE=$UBU_PATH" > ../.env ;;
+  msys*)    echo "SOURCE=$WIN_PATH" > ../.env ;;
+  *)        export HOST_OS="unknown: $OSTYPE" ;;
+esac
