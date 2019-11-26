e45ef451653ef1dd6bd31c013687de1b6c8e0693
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-11-26 16:51:39 +0530

    added weserver to compose

--- webserver/server.py
+++ webserver/server.py
@@ -102,5 +102,5 @@ def delete():
 
 if __name__ == "__main__":
     
-    port = os.environ.get("PORT", 5000)
-    app.run(debug = True, host="127.0.0.1", port=port)
+    port = os.environ.get("PORT", 5001)
+    app.run(debug = True, host="0.0.0.0", port=port)
