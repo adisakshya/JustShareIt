ee4ca99d5556a2d53d07f7d5d1c90d4b7ec79999
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-11-30 16:55:06 +0530

    modified start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,47 @@
-#!/bin/sh
+# !/bin/sh
+
+# JustShareIt! v1.0
+init() {
+
+    INTRO_HASH="$(base64 JustShareIt)"
+    while read line; do echo ${line} | base64 --decode; done <<< "$INTRO_HASH"
+
+}
+
+# Call to head function
+init
 
 # Set environment for docker-compose
-echo "Setting up environment..."
-echo "Creating reference mount..."
-sh ./compose_utils/set_compose_env.sh
+echo "[INFO] ==> Setting up environment..."
+echo "[INFO] ==> Creating reference mount..."
 
-# Docker-Compose
-echo "Getting Ready to JustShareIt!"
-docker-compose up --build
+# Configure the container for bind-mount
+# Sets variables in .env
+set_compose_env() {
+    
+    WIN_PATH="/c/Users"
+    UBU_PATH="/usr"
+    MAC_PATH=""
+    
+    # Update SOURCE path in .env for docker-compose.yml
+    case "$OSTYPE" in
+    darwin*)  echo "SOURCE=$MAC_PATH" > ./.env ;; 
+    linux*)   echo "SOURCE=$UBU_PATH" > ./.env ;;
+    msys*)    echo "SOURCE=$WIN_PATH" > ./.env ;;
+    *)        export HOST_OS="unknown: $OSTYPE" ;;
+    esac
+
+}
+
+# docker-compose
+compose() {
+
+    # Docker-Compose
+    echo "[INFO] ==> Getting things ready to JustShareIt!"
+    docker-compose up --build
+
+}
+
+# Call to set environment variables for docker-compose
+# and run docker-compose
+set_compose_env && compose
