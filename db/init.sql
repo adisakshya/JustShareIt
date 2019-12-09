8dca0e63249703acbc07a03276186cd049521d6f
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-12-09 20:31:55 +0530

    updated db for default admin config

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,13 @@
+-- CREATE DB
 CREATE DATABASE JustShareIt;
 use JustShareIt;
 
+-- CREATE ADMIN TABLE
 CREATE TABLE admin (
-  name VARCHAR(30) PRIMARY KEY,
+  name VARCHAR(30),
   email VARCHAR(30),
   password VARCHAR(255)
 );
 
+-- INSERT DEFAULT ADMIN
+INSERT INTO admin (name, email, password) VALUES ("JustShareItAdmin", NULL, "15493336")
