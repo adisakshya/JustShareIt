45c591000a604032406854f5ecc00029d37fc84e
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2019-12-09 17:55:06 +0530

    added base-URL redirect to client

--- webserver/server.py
+++ webserver/server.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, redirect, url_for
 from blueprints.client import user
 from blueprints.admin import administrator
 
 application = Flask(__name__)
 application.register_blueprint(user)
 application.register_blueprint(administrator)
+
+@application.route("/")
+def client_index():
+    return redirect(url_for("user.client_login"))
 
 port = os.environ.get("PORT", 5001)
 application.secret_key = os.urandom(12)
