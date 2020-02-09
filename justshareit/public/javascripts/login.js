4334c77ee6d0bf5c6e02df0dbd1d8f50160fd717
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-02-09 14:29:30 +0530

    documented login-js

--- justshareit/public/javascripts/login.js
+++ justshareit/public/javascripts/login.js
@@ -1,8 +1,11 @@
+/* On submition of request form  */
 $("#request_form").on("submit", function () {
     
     event.preventDefault();
     
     let data = $('#request_form').serialize();
+    
+    /* API Call */
     $.ajax({
         url: "/request",
         async: true,
@@ -16,11 +19,13 @@ $("#request_form").on("submit", function () {
             "cache-control": "no-cache"
         },
         success: function(response) {
-            if(response.success && response.error === false) {
+            /* On success update request button */
+            if(response.success && response.error === false) {               
                 var html = '<a class="btn btn-info btn-lg" role="button" data-toggle="modal" href="#" id="access_granted">Access Requested</a><p>You will be redirected to dashboard, once admin approves your request!</p>';
                 html = $.parseHTML(html);
                 $("#request-section").html(html);
                 $("#login").modal("hide");
+                /* Store username in session */
                 sessionStorage.SessionName = response.message.username;
             } else {
                 alert(response.message);
@@ -30,9 +35,12 @@ $("#request_form").on("submit", function () {
 
 });
 
+/* Verify user request */
 (function verify() {
+    /* Get username from session */
     var username = sessionStorage.getItem("SessionName");
     if(username) {
+        /* API Call */
         $.ajax({
             url: "/verify",
             async: false,
@@ -45,10 +53,13 @@ $("#request_form").on("submit", function () {
                 "cache-control": "no-cache"
             },
             success: async function(response) {
+                /* On no success erase username from session & redirect to login */
                 if (!response.success && response.error) {
                     sessionStorage.removeItem("SessionName");
                     window.location.href = '/';
                 }
+
+                /* On success redirect to client-index */
                 if(response.success && !response.error && response.message.verified && response.message.token) {
                     var html = '<form action="/client" method="post"><input type="text" name="username" value="' + response.message.username + '" hidden /><input type="text" name="token" value="' + response.message.token + '" hidden /><input id="index-form" type="submit" class="btn btn-success" value="Request Approved"></form>'
                     html = $.parseHTML(html);
