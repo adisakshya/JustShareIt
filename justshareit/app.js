838d3cd69c4192ec594f65a7db6bc97245ffc669
Adisakshya Chauhan <45028982+adisakshya@users.noreply.github.com>
2020-02-17 12:46:20 +0530

    moved socket-code to ./utils/socket.js

--- justshareit/app.js
+++ justshareit/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const fs = require('fs');
 const socket = require('socket.io');
 
 const usersRouter = require('./routes/users');
@@ -38,53 +37,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-/* Sockets Setup */
+/* Socket Setup */
 const io = socket();
 app.io = io;
-
-/* Define file structure */
-var files = {};
-
-/* On Connection */
-io.on('connection', function (socket) {
-
-  /* a new user connected */
-  socket.on("create", function () {
-    console.log("[SOCKET] => a new user connected");
-  });
-
-  /* file slice received from admin */
-  socket.on('slice', function (data) {
-
-    /* File hasn't been shared yet */
-    if(!files[data.name]) {
-      files[data.name] = data.size;
-    }
-
-    /* File has completely been transfered */
-    if(data.size < data.offset) {
-      return;
-    }
-
-    /* Forward slice to client */
-    socket.broadcast.emit('send slice', data);
-
-    /* Request next slice */
-    socket.emit('request slice', data.name, data.offset);
-
-  });
-
-  /* Admin request list of files shared */
-  socket.on('get files', function () {
-    /* Emit shared files information */
-    socket.emit('shared files', files);
-  });
-
-  /* on disconnect */
-  socket.on("disconnect", function () {
-    console.log("[SOCKET] => a user disconnected");
-  });
-
-});
+var sock = require('./utils/socket');
+sock.start(io);
 
 module.exports = app;
